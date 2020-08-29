589e506a4cf32a520ff98b70cc972de2e4962c36
rizkyrajitha <rajithagunathilake@gmail.com>
2020-08-29 14:06:19 +0530

    minor changes , tidy up 😉

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const fetch = require("node-fetch");
 const core = require("@actions/core");
 const fs = require("fs");
 const FormData = require("form-data");
+
 // add stealth plugin and use defaults (all evasion techniques)
 // const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 
@@ -33,43 +34,17 @@ const run = async () => {
   const formData = new FormData();
   formData.append("photo", readStream);
 
-  //   var formdata = new FormData();
-  //   formdata.append(
-  //     "photo",
-  //     fileInput.files[0],
-  //     "/C:/Users/Rajitha Gunathilake/Downloads/photo6179077395177384496.jpg"
-  //   );
-
-  //   var requestOptions = {
-  //     method: "POST",
-  //     body: formdata,
-  //     redirect: "follow",
-  //   };
-
-  //   fetch("https://api.telegram.org/bot98690c/sendPhoto?chat_id=-407", requestOptions)
-  //     .then(response => response.text())
-  //     .then(result => console.log(result))
-  //     .catch(error => console.log('error', error));
-
-  //   `https://api.telegram.org/bot${TELEGRAM_BOT_ID}/sendPhoto?chat_id=${TELEGRAM_CHAT_ID}
-
-  console.log('adadsa'+TELEGRAM_CHAT_ID);
-
   try {
     let data = await fetch(
       `https://api.telegram.org/${TELEGRAM_BOT_ID}/sendPhoto?chat_id=${TELEGRAM_CHAT_ID}`,
-      //   requestOptions
+
       {
         method: "POST",
         redirect: "follow",
-        // headers: {
-        //   "Content-length": fileSizeInBytes,
-        // },
-        body: formData, //bufferContent,
+
+        body: formData,
       }
     ).then((response) => response.json());
-
-    console.log(data);
 
     if (data.ok) {
       core.debug("success ✅");
